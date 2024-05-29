121f11cd7a94b294726436b117f6084d39d1ad3a
Bordless <38050217+Bordless-ita@users.noreply.github.com>
2024-05-29 14:24:01 +0200

    Versione 1.2 - Nuovi protocolli di cifratura
    Aggiunti protocolli di cifratura sovrapposti per garantire la massima criptazione del testo.
    Disabilitata funzione decipher! Da aggiornare al più presto,

--- protocollocifrario/cifratura.py
+++ protocollocifrario/cifratura.py
@@ -1,4 +1,5 @@
-import funzionicifratura, os, time
+import funzionicifratura, os, time, Color_Console
+Color_Console.color(text="green", bg="black")
 
 
 ex="F"
@@ -6,49 +7,59 @@ while ex!="V" :
     i=""
     sc1=""
     sc1a=""
+    os.system("cls")
     funzionicifratura.intestazione()
     funzionicifratura.menu()
     i=input("Effettua la tua scelta: ").upper()
     match i:
-        case "1":
+        case "1": #####DA FINIRE -- Funzione di Decriptazione
             os.system("cls")
             funzionicifratura.intestazione()
-            sc1=input("Inserisci messaggio da decriptare: ").upper()
+            '''sc1=input("Inserisci messaggio da decriptare: ").upper()
             os.system("cls")
             funzionicifratura.intestazione()
-            print("Messaggio decriptato:",'"',funzionicifratura.decriptabase(sc1),'"')
+            print("Messaggio decriptato:",'"',funzionicifratura.scompositore(funzionicifratura.decriptabase(sc1)),'"')
+            print("Cifratura intermedia:","'",funzionicifratura.decriptabase(sc1),"'")
             print("Messaggio criptato:",'"',sc1,'"')
             sca=input("Inserire qualsiasi valore per tornare al menu: ")
             if sca.isascii():
                 os.system("cls")
                 funzionicifratura.intestazione()
                 print("Ritorno al menù...")
-                time.sleep(5)
-                os.system("cls")
+                time.sleep(3)
+                os.system("cls")'''
+            print("Funzione WIP!")
+            time.sleep(3)
         case "2":
             os.system("cls")
             funzionicifratura.intestazione()
             sc2=input("Inserisci messaggio da criptare: ").upper()
             os.system("cls")
             funzionicifratura.intestazione()
-            print("Messaggio criptato:","'",funzionicifratura.criptabase(sc2),"'")
+            print("Messaggio criptato:","'",funzionicifratura.convertitore(funzionicifratura.germanico(funzionicifratura.criptabase(funzionicifratura.albam(funzionicifratura.atbah(funzionicifratura.atbash(funzionicifratura.cesare(sc2,3))))))),"'")
+            print("Criptazione Germanica:","'",funzionicifratura.germanico(funzionicifratura.criptabase(funzionicifratura.albam(funzionicifratura.atbah(funzionicifratura.atbash(funzionicifratura.cesare(sc2,3)))))),"'")
+            print("Criptazione intermedia:","'",funzionicifratura.criptabase(funzionicifratura.albam(funzionicifratura.atbah(funzionicifratura.atbash(funzionicifratura.cesare(sc2,3))))),"'")
+            print("Criptazione ALBAM:","'",funzionicifratura.albam(funzionicifratura.atbah(funzionicifratura.atbash(funzionicifratura.cesare(sc2,3)))),"'")
+            print("Criptazione ATBAH:","'",funzionicifratura.atbah(funzionicifratura.atbash(funzionicifratura.cesare(sc2,3))),"'")
+            print("Criptazione ATBASH:","'",funzionicifratura.atbash(funzionicifratura.cesare(sc2,3)),"'")
+            print("Criptazione Cesare:","'",funzionicifratura.cesare(sc2,3),"'")
             print("Messaggio decriptato:","'",sc2,"'")
             sca=input("Inserire qualsiasi valore per tornare al menu: ")
             if sca.isascii():
                 os.system("cls")
                 funzionicifratura.intestazione()
                 print("Ritorno al menù...")
-                time.sleep(5)
+                time.sleep(3)
                 os.system("cls")
-
         case "E":
             os.system("cls")
             funzionicifratura.intestazione()
             print("Uscita in corso...")
-            time.sleep(5)
+            time.sleep(3)
             ex="V"
         case _:
+            os.system("cls")
             funzionicifratura.intestazione()
             print("Errore! Comando non riconosciuto\nRitorno al menù principale...")
-            time.sleep(5)
+            time.sleep(3)
             os.system("cls")
--- protocollocifrario/funzionicifratura.py
+++ protocollocifrario/funzionicifratura.py
@@ -1,16 +1,16 @@
 #Cifratura Base#
 
 def intestazione():
-    print("                 DECIPHER V1")
-    print("            di Marco Lo Giudice")
-    print("################################################")
+    print("                   CIPHER V1")
+    print("              di Marco Lo Giudice")
+    print("######################################################")
 
 def menu():
-    print("************************************************")
-    print("* 1 per avviare il protocollo di decriptazione.*")
-    print("* 2 per avviare il protocollo di criptazione.  *")
-    print("* E per uscire dal programma.                  *")
-    print("************************************************")
+    print("******************************************************")
+    print("* 1 per avviare il protocollo di decriptazione. WIP! *")
+    print("* 2 per avviare il protocollo di criptazione.        *")
+    print("* E per uscire dal programma.                        *")
+    print("******************************************************")
 
 def decriptabase(parola):
     cifrato = ['J', '?', 'W', 'B', ':', 'Q', 'H', '.', 'Z', 'X', 'R', ';', 'N', 'T', ' ', '7', '3', '9', '1', '4', '2', '6', '0', '5', '8', 'C', 'A', 'O', 'S', '!', 'D', 'G', 'I', 'M', 'P', 'E', ',', 'F', 'U', 'L', 'Y', 'K', 'V']
@@ -52,3 +52,107 @@ def criptabase(parola):
             #risultato2.append('?')
 
     return ''.join(risultato2)
+
+##DA FINIRE## http://www.fogazzaro.it/ISA/5AT/Crittografia.pdf
+'''def criptaavanzata(risultato):
+    risultato3=[]
+    a=list(range(33,97))
+    for n in a:
+        risultato3.append(chr(n))
+        indice=risultato.index()
+'''
+
+
+def convertitore(risultato):
+    risultato4 = []
+    for char in risultato:
+        risultato4.append(str(ord(char)))
+    return ''.join(risultato4)
+
+def scompositore(risultato_ascii):
+    risultato5 = []
+    i = 0
+    while i < len(risultato_ascii):
+        if i + 2 < len(risultato_ascii) and risultato_ascii[i:i+3].isdigit() and 32 <= int(risultato_ascii[i:i+3]) <= 126:
+            # Prova a prendere 3 cifre
+            risultato5.append(chr(int(risultato_ascii[i:i+3])))
+            i += 3
+        elif risultato_ascii[i:i+2].isdigit():
+            # Prendi 2 cifre
+            risultato5.append(chr(int(risultato_ascii[i:i+2])))
+            i += 2
+    return ''.join(risultato5)
+
+def atbash(parola):
+    chiaro = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    cifrato = 'ZYXWVUTSRQPONMLKJIHGFEDCBA'
+    
+    risultato = []
+    for char in parola.upper():
+        if char in chiaro:
+            index = chiaro.index(char)
+            risultato.append(cifrato[index])
+        else:
+            risultato.append(char)
+    
+    return ''.join(risultato)
+
+def albam(parola):
+    chiaro = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    cifrato = 'NOPQRSTUVWXYZABCDEFGHIJKLM'
+    
+    risultato = []
+    for char in parola.upper():
+        if char in chiaro:
+            index = chiaro.index(char)
+            risultato.append(cifrato[index])
+        else:
+            risultato.append(char)
+    
+    return ''.join(risultato)
+
+def atbah(parola):
+    chiaro = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    cifrato = 'TSRQPONMLKJIHGFEDCBAUVWXYZ'
+    
+    risultato = []
+    for char in parola.upper():
+        if char in chiaro:
+            index = chiaro.index(char)
+            risultato.append(cifrato[index])
+        else:
+            risultato.append(char)
+    
+    return ''.join(risultato)
+
+
+def cesare(parola, shift):
+    chiaro = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    risultato = []
+
+    for char in parola.upper():
+        if char in chiaro:
+            index = (chiaro.index(char) + shift) % 26
+            risultato.append(chiaro[index])
+        else:
+            risultato.append(char)
+
+    return ''.join(risultato)
+
+
+
+
+def germanico(parola):
+    cifrato = ['J', '?', 'W', 'B', ':', 'Q', 'H', '.', 'Z', 'X', 'R', ';', 'N', 'T', ' ', '7', '3', '9', '1', '4', '2', '6', '0', '5', '8', 'C', 'A', 'O', 'S', '!', 'D', 'G', 'I', 'M', 'P', 'E', ',', 'F', 'U', 'L', 'Y', 'K', 'V']
+    chiaro = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '!', '?', '.', ',', ';', ':', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+    risultato = []
+
+    for char in parola:
+        if char in chiaro:
+            index = cifrato.index(char)
+            risultato.append(chiaro[index])
+        else:
+            risultato.append(char)
+
+    return ''.join(risultato)
